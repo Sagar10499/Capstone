4f3ea65d338bd8be270362d15c309736aa55ada7
Sagar10499 <46324056+Sagar10499@users.noreply.github.com>
2024-03-22 11:14:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_FJpHfOX8t', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7of5rhhnk1vsmfq9o1580cork0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_Wf5iIDiRM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ii39hsfd59r7bofsgrb2cnqa9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://3lne2fydok.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
